dc4125d82b63067d293869e7d09cd0fa9129315b
Severin Fernando Tanga Mvondo <93095402+Jarodak47@users.noreply.github.com>
2022-02-10 23:06:26 +0100

    RAS

--- app/main/models/database/session.py
+++ app/main/models/database/session.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #DATABASE_URL = "sqlite:///./sql_app.db"
-DATABASE_URL = "postgresql://mvondofernando7777:jarodak47@localhost:5432/hotel"
+SQLALCHEMY_DATABASE_URI = "postgresql://mvondofernando7777:jarodak47@localhost:5432/hotel"
 
 engine = create_engine(
-    DATABASE_URL
+    SQLALCHEMY_DATABASE_URI
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
